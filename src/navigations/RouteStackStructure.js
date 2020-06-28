a546272aa0cc9a57edf53d3fb6d0b856f47bb25f
Ariel Sandor <arielsandor>
2020-06-28 13:56:11 -0300

    logout screen

--- src/navigations/RouteStackStructure.js
+++ src/navigations/RouteStackStructure.js
@@ -5,6 +5,7 @@ import HomeScreen from "../screens/HomeScreen";
 import DetailsScreen from "../screens/DetailsScreen";
 import RegisterScreen from "../screens/RegisterScreen";
 import LoginScreen from "../screens/LoginScreen";
+import LogoutScreen from "../screens/LogoutScreen";
 
 const Stack = createStackNavigator();
 
@@ -74,7 +75,31 @@ export const loginScreen = ({ navigation }) => {
         name="Login"
         component={LoginScreen}
         options={{
-          title: "Login", //Set Header Title
+          title: "Iniciar sesion", //Set Header Title
+          headerLeft: () => (
+            <MenuDrawerStructure navigationProps={navigation} />
+          ),
+          headerStyle: {
+            backgroundColor: "#cfcba7", //Set Header color
+          },
+          headerTintColor: "#333", //Set Header text color
+          headerTitleStyle: {
+            fontWeight: "bold", //Set Header text style
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export const logoutScreen = ({ navigation }) => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Logout"
+        component={LogoutScreen}
+        options={{
+          title: "Cerrar sesion", //Set Header Title
           headerLeft: () => (
             <MenuDrawerStructure navigationProps={navigation} />
           ),
---
+++ src/screens/LogoutScreen.js
@@ -0,0 +1,141 @@
+import React, { Component, useState, useEffect } from "react";
+import { StyleSheet, View, Text, Image } from "react-native";
+import { Button } from "react-native-elements";
+import AsyncStorage from "@react-native-community/async-storage";
+
+
+function LogoutScreen(props) {
+
+    const [loginName, setLoginName] = useState("");
+    const [loginEmail, setLoginEmail] = useState("");
+
+    useEffect(() => {
+        checkUserSignedIn();
+    }, []);
+
+    checkUserSignedIn = async () =>{
+        try {
+            let value = await AsyncStorage.getItem("@user");
+            console.log("USER DATA: " + value);
+            if (value != null){
+                console.log("LOGGED IN");
+                let data = JSON.parse(value);
+                setLoginName(data.fullName);
+                setLoginEmail(data.email);
+            }
+            else {
+                console.log("NOT LOGGED IN");
+                setLoginName("No Logueado");
+                setLoginEmail("No Logueado");
+            }
+        } catch (error) {
+            console.log(error);
+            console.log("ERROR GETTING USER DATA");
+        }
+    }
+
+    const logoutUser = async () => {
+        AsyncStorage.removeItem("@user");
+        console.log(loginName);
+        checkUserSignedIn();
+        props.navigation.navigate("Home", {
+            flashMessage: "Sesion Cerrada con Exito!",
+        });
+     };
+
+  return (
+    <View style={styles.container}>
+        <View style={styles.materialButtonViolet}>
+            <Button
+                title="Cerrar Sesion"
+                buttonStyle={styles.btnStyle}
+                onPress={logoutUser}
+            />
+        </View>
+      <Text style={styles.loremIpsum}>
+        Estas seguro de querer cerrar sesion?
+      </Text>
+      <View style={styles.nombreRow}>
+        <Text style={styles.nombre}>Nombre:</Text>
+        <Text style={styles.loremIpsum3}>{loginName}</Text>
+      </View>
+      <View style={styles.emailRow}>
+        <Text style={styles.email}>Email:</Text>
+        <Text style={styles.loremIpsum5}>{loginEmail}</Text>
+      </View>
+      <Image
+        source={require("../../assets/loadingIcon.png")}
+        resizeMode="contain"
+        style={styles.image}
+      ></Image>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(4,80,168,1)"
+  },
+  btnStyle: {
+    marginTop: 10,
+    justifyContent: "center",
+    height: 60,
+  },
+  sendComment: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  materialButtonViolet: {
+    height: 78,
+    width: 174,
+    marginTop: 367,
+    alignSelf: "center"
+  },
+  loremIpsum: {
+    color: "rgba(255,255,255,1)",
+    fontSize: 18,
+    marginTop: -270,
+    marginLeft: 37
+  },
+  nombre: {
+    color: "rgba(255,255,255,1)",
+    fontSize: 17
+  },
+  loremIpsum3: {
+    color: "rgba(255,255,255,1)",
+    fontSize: 17,
+    marginLeft: 12
+  },
+  nombreRow: {
+    height: 20,
+    flexDirection: "row",
+    marginTop: 55,
+    marginLeft: 61,
+    marginRight: 138
+  },
+  email: {
+    color: "rgba(255,255,255,1)",
+    fontSize: 17
+  },
+  loremIpsum5: {
+    color: "rgba(255,255,255,1)",
+    fontSize: 17,
+    marginLeft: 31
+  },
+  emailRow: {
+    height: 20,
+    flexDirection: "row",
+    marginTop: 28,
+    marginLeft: 61,
+    marginRight: 138
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginTop: 182,
+    marginLeft: 87
+  }
+});
+
+export default LogoutScreen;
