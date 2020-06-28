5e4937044d84a77c2b208dedb07f9a277e75ef34
Julia Espinoza <juespinozacorrea@gmail.com>
2020-06-28 00:59:44 -0300

    Added tabs to the lists of movies

--- src/screens/MovieListsScreen.js
+++ src/screens/MovieListsScreen.js
@@ -1,169 +1,55 @@
-import React from "react";
-import AsyncStorage from "@react-native-community/async-storage";
-import {
-  SafeAreaView,
-  Dimensions,
-  StyleSheet,
-  FlatList,
-  TouchableOpacity,
-} from "react-native";
-import { Text, Block, Card, Icon, Button } from "galio-framework";
-import { getAllLists } from "../controllers/MovieListController";
-import theme from "../theme";
+import "react-native-gesture-handler";
+import * as React from "react";
+import { View, StyleSheet, Dimensions } from "react-native";
+import { TabView, SceneMap, TabBar } from "react-native-tab-view";
+import PublicListsScreen from "./PublicListsScreen";
 
-const { height, width } = Dimensions.get("window");
+const SecondRoute = () => (
+  <View style={[styles.scene, { backgroundColor: "#fff" }]} />
+);
 
-export default class MovieLists extends React.Component {
-  _isMounted = false;
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentUser: null,
-      movieLists: [],
-      isLoading: true,
-    };
-  }
+const ThirdRoute = () => (
+  <View style={[styles.scene, { backgroundColor: "#fff" }]} />
+);
 
-  componentDidMount() {
-    this._isMounted = true;
-    this.setCurrentUser();
-    this.getMovies();
-  }
+const initialLayout = { width: Dimensions.get("window").width };
 
-  componentWillUnmount() {
-    this._isMounted = false;
-  }
+export default function MovieListsScreen() {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: "first", title: "Públicas" },
+    { key: "second", title: "Mis listas" },
+    { key: "third", title: "Siguiendo" },
+  ]);
 
-  setCurrentUser = () => {
-    const currentUser = this.getData();
-    if (this._isMounted) {
-      this.setState({ currentUser });
-    }
-  };
+  const renderScene = SceneMap({
+    first: PublicListsScreen,
+    second: SecondRoute,
+    third: ThirdRoute,
+  });
 
-  getData = async () => {
-    try {
-      const jsonValue = await AsyncStorage.getItem("@user");
-      return jsonValue != null ? JSON.parse(jsonValue) : null;
-    } catch (e) {
-      console.error("Error getting logged user data with AsyncStorage");
-    }
-  };
+  const renderTabBar = (props) => (
+    <TabBar
+      {...props}
+      indicatorStyle={{ backgroundColor: "#666" }}
+      style={{ backgroundColor: "#fff" }}
+      labelStyle={{ color: "#666" }}
+    />
+  );
 
-  getMovies = async () => {
-    // TODO: call to api
-    let response = await getAllLists();
-    if (this._isMounted && response.rdo == 0) {
-      this.setState({ movieLists: response.data, isLoading: false });
-    } else {
-      console.error("Was not able to get movie lists");
-    }
-  };
-
-  renderListItem = ({ item }) => {
-    return (
-      <TouchableOpacity onPress={() => console.log(item._id)}>
-        <Card
-          flex
-          center
-          borderless
-          shadowColor={theme.COLORS.BLACK}
-          style={styles.author}
-          title={item.name}
-          caption={item.owner}
-          avatar="http://lorempixel.com/80/80/abstract/" //"https://picsum.photos/80"
-          location={
-            <Block row>
-              <Block row middle style={{ marginHorizontal: theme.SIZES.BASE }}>
-                <Icon
-                  name={item.public ? "eye" : "eye-close"}
-                  family="font-awesome"
-                  color={theme.COLORS.MUTED}
-                  size={theme.SIZES.FONT * 0.875}
-                />
-                <Text
-                  p
-                  color={theme.COLORS.MUTED}
-                  size={theme.SIZES.FONT * 0.875}
-                  style={{ marginLeft: theme.SIZES.BASE * 0.25 }}
-                >
-                  {item.public ? "Publico" : "Privado"}
-                </Text>
-              </Block>
-              <Block row middle>
-                <Icon
-                  name="film"
-                  family="font-awesome"
-                  color={theme.COLORS.MUTED}
-                  size={theme.SIZES.FONT * 0.875}
-                />
-                <Text
-                  p
-                  color={theme.COLORS.MUTED}
-                  size={theme.SIZES.FONT * 0.875}
-                  style={{ marginLeft: theme.SIZES.BASE * 0.25 }}
-                >
-                  {item.movies.length}
-                </Text>
-              </Block>
-            </Block>
-          }
-        />
-      </TouchableOpacity>
-    );
-  };
-
-  render() {
-    const { navigation } = this.props;
-    const { movieLists, isLoading, currentUser } = this.state;
-    return (
-      <SafeAreaView style={styles.container}>
-        {currentUser !== null && (
-          <Block flex={3}>
-            <Block center style={{ paddingTop: 20 }}>
-              <Text muted>Todas las listas públicas</Text>
-            </Block>
-            {isLoading && (
-              <Text flex center>
-                Cargando películas...
-              </Text>
-            )}
-            {!isLoading && (
-              <Block>
-                {movieLists.length > 0 && (
-                  <FlatList
-                    data={movieLists}
-                    renderItem={this.renderListItem}
-                    keyExtractor={(item) => item._id}
-                  />
-                )}
-                {movieLists.length === 0 && (
-                  <Text flex center>
-                    No hay películas públicas.
-                  </Text>
-                )}
-              </Block>
-            )}
-          </Block>
-        )}
-        {currentUser === null && (
-          <Button flex center onPress={() => navigation.navigate("Login")}>
-            <Text flex center muted>
-              Tienes que estar logueado!
-            </Text>
-          </Button>
-        )}
-      </SafeAreaView>
-    );
-  }
+  return (
+    <TabView
+      renderTabBar={renderTabBar}
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={initialLayout}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  scene: {
     flex: 1,
-  },
-  author: {
-    marginTop: 15,
-    width: width * 0.9,
   },
 });
---
+++ src/screens/PublicListsScreen.js
@@ -0,0 +1,169 @@
+import React from "react";
+import AsyncStorage from "@react-native-community/async-storage";
+import {
+  SafeAreaView,
+  Dimensions,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { Text, Block, Card, Icon, Button } from "galio-framework";
+import { getAllLists } from "../controllers/MovieListController";
+import theme from "../theme";
+
+const { height, width } = Dimensions.get("window");
+
+export default class PublicListsScreen extends React.Component {
+  _isMounted = false;
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: null,
+      movieLists: [],
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    this._isMounted = true;
+    this.setCurrentUser();
+    this.getMovies();
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+  setCurrentUser = () => {
+    const currentUser = this.getData();
+    if (this._isMounted) {
+      this.setState({ currentUser });
+    }
+  };
+
+  getData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem("@user");
+      return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch (e) {
+      console.error("Error getting logged user data with AsyncStorage");
+    }
+  };
+
+  getMovies = async () => {
+    // TODO: call to api
+    let response = await getAllLists();
+    if (this._isMounted && response.rdo == 0) {
+      this.setState({ movieLists: response.data, isLoading: false });
+    } else {
+      console.error("Was not able to get movie lists");
+    }
+  };
+
+  renderListItem = ({ item }) => {
+    return (
+      <TouchableOpacity onPress={() => console.log(item._id)}>
+        <Card
+          flex
+          center
+          borderless
+          shadowColor={theme.COLORS.BLACK}
+          style={styles.author}
+          title={item.name}
+          caption={item.owner}
+          avatar="http://lorempixel.com/80/80/abstract/" //"https://picsum.photos/80"
+          location={
+            <Block row>
+              <Block row middle style={{ marginHorizontal: theme.SIZES.BASE }}>
+                <Icon
+                  name={item.public ? "eye" : "eye-close"}
+                  family="font-awesome"
+                  color={theme.COLORS.MUTED}
+                  size={theme.SIZES.FONT * 0.875}
+                />
+                <Text
+                  p
+                  color={theme.COLORS.MUTED}
+                  size={theme.SIZES.FONT * 0.875}
+                  style={{ marginLeft: theme.SIZES.BASE * 0.25 }}
+                >
+                  {item.public ? "Publico" : "Privado"}
+                </Text>
+              </Block>
+              <Block row middle>
+                <Icon
+                  name="film"
+                  family="font-awesome"
+                  color={theme.COLORS.MUTED}
+                  size={theme.SIZES.FONT * 0.875}
+                />
+                <Text
+                  p
+                  color={theme.COLORS.MUTED}
+                  size={theme.SIZES.FONT * 0.875}
+                  style={{ marginLeft: theme.SIZES.BASE * 0.25 }}
+                >
+                  {item.movies.length}
+                </Text>
+              </Block>
+            </Block>
+          }
+        />
+      </TouchableOpacity>
+    );
+  };
+
+  render() {
+    const { navigation } = this.props;
+    const { movieLists, isLoading, currentUser } = this.state;
+    return (
+      <SafeAreaView style={styles.container}>
+        {currentUser !== null && (
+          <Block flex={3}>
+            <Block center style={{ paddingTop: 20 }}>
+              <Text muted>Todas las listas públicas</Text>
+            </Block>
+            {isLoading && (
+              <Text flex center>
+                Cargando películas...
+              </Text>
+            )}
+            {!isLoading && (
+              <Block>
+                {movieLists.length > 0 && (
+                  <FlatList
+                    data={movieLists}
+                    renderItem={this.renderListItem}
+                    keyExtractor={(item) => item._id}
+                  />
+                )}
+                {movieLists.length === 0 && (
+                  <Text flex center>
+                    No hay películas públicas.
+                  </Text>
+                )}
+              </Block>
+            )}
+          </Block>
+        )}
+        {currentUser === null && (
+          <Button flex center onPress={() => navigation.navigate("Login")}>
+            <Text flex center muted>
+              Tienes que estar logueado!
+            </Text>
+          </Button>
+        )}
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  author: {
+    marginTop: 15,
+    width: width * 0.9,
+  },
+});
